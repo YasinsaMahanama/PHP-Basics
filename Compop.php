a8bc81aabfa7acd5d3a16040e347be80aea2c158
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 18:51:39 +0530

    Update Compop.php

--- Compop.php
+++ Compop.php
@@ -1,33 +1,47 @@
-<?php
+<html>
+  <head>
 
-$a = 42;
-$b = 20;
+    <title>PHP Test</title>
+    
+  </head>
 
-if($a == $b)
-{
-  echo "a is equal to b <br/>";
-}
-else
-{
-  echo "a is not equal to b <br/>";
-}
+  <body>
 
-if($a > $b)
-{
-  echo "a is greater than to b <br/>";
-}
-else
-{
-  echo "a is not greater than to b <br/>";
-}
+  <?php
 
-if($a < $b)
-{
-  echo "a is less than to b <br/>";
-}
-else
-{
-  echo "a is not less than to b <br/>";
-}
+  $a = 42;
+  $b = 20;
+  
+  if($a == $b)
+  {
+    echo "a is equal to b <br/>";
+  }
+  else
+  {
+    echo "a is not equal to b <br/>";
+  }
+  
+  if($a > $b)
+  {
+    echo "a is greater than to b <br/>";
+  }
+  else
+  {
+    echo "a is not greater than to b <br/>";
+  }
+  
+  if($a < $b)
+  {
+    echo "a is less than to b <br/>";
+  }
+  else
+  {
+    echo "a is not less than to b <br/>";
+  }
+  
+  ?>
 
-?>
+    
+  </body>
+</html>
+
