fb5540385b7861cd0d619e95f87368d7a5a83535
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 18:38:21 +0530

    Create Assignop.php

---
+++ Assignop.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 42;
+$b = 20;
+
+$c = $a + $b;
+echo "Addition Operation Result : $c <br/>";
+
+$c += $a;
+echo "Addition And Assignment Operation Result : $c <br/>";
+
+$c -= $a;
+echo "Subtraction And Assignment Operation Result : $c <br/>";
+
+$c *= $a;
+echo "Multiplication And Assignment Operation Result : $c <br/>";
+
+$c /= $a / $b;
+echo "Divition And Assignment Operation Result : $c <br/>";
+
+$c %= $a % $b;
+echo "Modulus And Assignment Operation Result : $c <br/>";
+
+?>
