a4d34a48f01c0852f0d706d08e48193480399829
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 17:50:52 +0530

    Create Arithop.php

---
+++ Arithop.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 42;
+$b = 20;
+
+$c = $a + $b;
+echo "Addition Operation Result : $c <br/>";
+
+$c = $a - $b;
+echo "Subtraction Operation Result : $c <br/>";
+
+$c = $a * $b;
+echo "Multiplication Operation Result : $c <br/>";
+
+$c = $a / $b;
+echo "Divition Operation Result : $c <br/>";
+
+$c = $a % $b;
+echo "Modulus Operation Result : $c <br/>";
+
+$c = $a++;
+echo "Increment Operation Result : $c <br/>";
+
+$c = $a--;
+echo "Decrement Operation Result : $c <br/>";
+
+?>
