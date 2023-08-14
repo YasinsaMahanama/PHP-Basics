96c71f570afee55202e1712cb5e93dde937d9ab7
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 19:44:07 +0530

    Create Defaultag.php

---
+++ Defaultag.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+
+    <title>User defined function Argument</title>
+    
+  </head>
+
+  <body>
+
+    <?php
+
+    function percentage($p, $c, $m, $ttl=300)
+    {
+      echo "p=$p, c=$c, m=$m, ttl=$ttl<br/>";
+
+      $percent = ($p+$c+$m)*100/$ttl;
+
+      echo "Percentage = $percent <br/>";
+    }
+
+    percentage(50,60,70);
+    percentage(25,30,35,150);
+
+    ?>
+    
+  </body>
+</html>
+
