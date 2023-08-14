4b1458685dd93c819daec192af4642ba7edaf813
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 21:53:27 +0530

    Create retvalue.php

---
+++ retvalue.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+
+    <title>User defined function return value</title>
+    
+  </head>
+
+  <body>
+
+    <?php
+
+    function average($args)
+      {
+        print_r($args);
+        echo "<br/>";
+        $sum = 0;
+        $count = count($args);
+        for($i=0;$i<$count;$i++)
+          {
+            $sum +=$args[$i];
+          }
+        $average = $sum/$count;
+        return $average;
+        
+      }
+      $numbers = array(10,20,30,40,50);
+      $average = average($numbers);
+      echo "Average = $average <br/>";
+
+    ?>
+    
+  </body>
+</html>
+
