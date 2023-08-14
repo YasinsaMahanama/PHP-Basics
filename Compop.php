22f2295328253ce33fd4c59081ade880d33401ab
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 18:32:03 +0530

    Create Compop.php

---
+++ Compop.php
@@ -0,0 +1,33 @@
+<?php
+
+$a = 42;
+$b = 20;
+
+if($a == $b)
+{
+  echo "a is equal to b <br/>";
+}
+else
+{
+  echo "a is not equal to b <br/>";
+}
+
+if($a > $b)
+{
+  echo "a is greater than to b <br/>";
+}
+else
+{
+  echo "a is not greater than to b <br/>";
+}
+
+if($a < $b)
+{
+  echo "a is less than to b <br/>";
+}
+else
+{
+  echo "a is not less than to b <br/>";
+}
+
+?>
