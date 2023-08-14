81d84ed55df7def56f76d012f6958b80f1d3c8c5
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-08-14 19:09:06 +0530

    Create Swap.php

---
+++ Swap.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+
+    <title>Call By Reference</title>
+    
+  </head>
+
+  <body>
+
+    <?php
+
+    function swap($x, $y)
+      {
+        $t = $x;
+        $x = $y;
+        $y = $t;
+      }
+
+      $a = 10;
+      $b = 15;
+
+      echo " Before the swap : a = $a  b = $b <br/>";
+
+      swap($a, $b);
+
+      echo " After the swap : a = $a  b = $b <br/>";
+
+    ?>
+    
+  </body>
+</html>
+
