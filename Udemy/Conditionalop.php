31646f11a2fbb86239f36f369a9175271d2acaed
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-15 22:22:11 +0530

    Create Conditionalop.php

---
+++ Udemy/Conditionalop.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+
+    <title>PHP Test</title>
+    
+  </head>
+
+  <body>
+
+    <?php
+
+        $a = 10;
+        $b = 12;
+
+        $result = ($a > $b)? $a : $b;
+
+        echo "TEST I : The max value is $result. <br/><br/>";
+
+        $result = ($a < $b)? $a : $b;
+
+        echo "TEST II : The min value is $result. <br/><br/>";
+
+    ?>
+    
+  </body>
+</html>
