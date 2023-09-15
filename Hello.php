32409739e1287a46d9d97f0088e91fbe93b8cc80
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-15 19:00:47 +0530

    Update Hello.php

--- Hello.php
+++ Hello.php
@@ -1,13 +1,11 @@
-<html>
-  <head>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Test</title>
-    
-  </head>
-
-  <body>
-
-    <?php echo "<h1>Hello  World!</h1>"; ?>
-    
-  </body>
+</head>
+<body>
+    <?php echo "<h1>Hello World!</h1>"; ?>
+</body>
 </html>
