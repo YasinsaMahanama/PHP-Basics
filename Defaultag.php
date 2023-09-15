e78e0d793054bf5ac3e3ff82587fd489ae691260
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-15 19:35:21 +0530

    Update Defaultag.php

--- Defaultag.php
+++ Defaultag.php
@@ -9,17 +9,17 @@
 
     <?php
 
-    function percentage($p, $c, $m, $ttl=300)
-    {
-      echo "p=$p, c=$c, m=$m, ttl=$ttl<br/>";
-
-      $percent = ($p+$c+$m)*100/$ttl;
-
-      echo "Percentage = $percent <br/>";
-    }
-
-    percentage(50,60,70);
-    percentage(25,30,35,150);
+      function percentage($p, $c, $m, $ttl=300)
+      {
+        echo "p=$p, c=$c, m=$m, ttl=$ttl<br/>";
+  
+        $percent = ($p+$c+$m)*100/$ttl;
+  
+        echo "Percentage = $percent <br/>";
+      }
+  
+      percentage(50,60,70);
+      percentage(25,30,35,150);
 
     ?>
     
