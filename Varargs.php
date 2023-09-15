636dfe4c65bab4334c9ef9d82f830c5f8f37f072
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-15 19:37:08 +0530

    Update Varargs.php

--- Varargs.php
+++ Varargs.php
@@ -9,21 +9,21 @@
 
     <?php
 
-    function average(&$args)
-      {
-        print_r($args);
-        echo "<br/>";
-        $sum = 0;
-        $count = count($args);
-        for($i=0;$i<$count;$i++)
-          {
-            $sum +=$args[$i];
-          }
-        $average = $sum/$count;
-        echo "Average = $average <br/>";
-      }
-      $numbers = array(10,20,30,40,50);
-      average($numbers);
+      function average(&$args)
+        {
+          print_r($args);
+          echo "<br/>";
+          $sum = 0;
+          $count = count($args);
+          for($i=0;$i<$count;$i++)
+            {
+              $sum +=$args[$i];
+            }
+          $average = $sum/$count;
+          echo "Average = $average <br/>";
+        }
+        $numbers = array(10,20,30,40,50);
+        average($numbers);
 
     ?>
     
