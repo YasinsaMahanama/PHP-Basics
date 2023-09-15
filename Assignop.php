531afa13239aeff9cd5582d520c1a5057d888cdc
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-15 19:28:05 +0530

    Update Assignop.php

--- Assignop.php
+++ Assignop.php
@@ -1,37 +1,34 @@
-<html>
-  <head>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Test</title>
-    
-  </head>
-
-  <body>
-
+</head>
+<body>
     <?php
 
-$a = 42;
-$b = 20;
+        $a = 42;
+        $b = 20;
 
-$c = $a + $b;
-echo "Addition Operation Result : $c <br/>";
+        $c = $a + $b;
+        echo "Addition Operation Result : $c <br/><br/>";
 
-$c += $a;
-echo "Addition And Assignment Operation Result : $c <br/>";
+        $c += $a;
+        echo "Addition And Assignment Operation Result : $c <br/><br/>";
 
-$c -= $a;
-echo "Subtraction And Assignment Operation Result : $c <br/>";
+        $c -= $a;
+        echo "Subtraction And Assignment Operation Result : $c <br/><br/>";
 
-$c *= $a;
-echo "Multiplication And Assignment Operation Result : $c <br/>";
+        $c *= $a;
+        echo "Multiplication And Assignment Operation Result : $c <br/><br/>";
 
-$c /= $a / $b;
-echo "Divition And Assignment Operation Result : $c <br/>";
+        $c /= $a / $b;
+        echo "Divition And Assignment Operation Result : $c <br/><br/>";
 
-$c %= $a % $b;
-echo "Modulus And Assignment Operation Result : $c <br/>";
+        $c %= $a % $b;
+        echo "Modulus And Assignment Operation Result : $c <br/><br/>";
 
-?>
-    
-  </body>
+    ?>
+</body>
 </html>
-
