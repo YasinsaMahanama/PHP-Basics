66ad9677be1832f062f4e3d8e7846c62d87b383b
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-15 19:38:21 +0530

    Update retvalue.php

--- retvalue.php
+++ retvalue.php
@@ -8,24 +8,24 @@
   <body>
 
     <?php
-
-    function average($args)
-      {
-        print_r($args);
-        echo "<br/>";
-        $sum = 0;
-        $count = count($args);
-        for($i=0;$i<$count;$i++)
-          {
-            $sum +=$args[$i];
-          }
-        $average = $sum/$count;
-        return $average;
-        
-      }
-      $numbers = array(10,20,30,40,50);
-      $average = average($numbers);
-      echo "Average = $average <br/>";
+  
+      function average($args)
+        {
+          print_r($args);
+          echo "<br/>";
+          $sum = 0;
+          $count = count($args);
+          for($i=0;$i<$count;$i++)
+            {
+              $sum +=$args[$i];
+            }
+          $average = $sum/$count;
+          return $average;
+          
+        }
+        $numbers = array(10,20,30,40,50);
+        $average = average($numbers);
+        echo "Average = $average <br/>";
 
     ?>
     
