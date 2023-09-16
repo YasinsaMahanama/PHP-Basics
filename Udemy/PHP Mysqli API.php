267527093b5ff24815ed3585cb7f3eaa312004d1
Yasinsa Mahanama <122031127+YasinsaMahanama@users.noreply.github.com>
2023-09-16 12:38:52 +0530

    Update and rename PHP Mysql API.php to PHP Mysqli API.php

--- Udemy/PHP Mysqli API.php
+++ Udemy/PHP Mysqli API.php
@@ -11,48 +11,53 @@
     $dbuser = 'root';
     $dbpass = '';
 
+    // Create a mysqli connection
     $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
     if (! $conn ) {
-        die('Could not connect: ' . mysqli_error($mysqli)); // Added $conn here
+        die('Could not connect: ' . mysqli_connect_error());
     }
 
     echo 'Connected successfully<br>';
 
-    $sql = 'CREATE DATABASE testdb';
+    // Create the database
+    $sql = 'CREATE DATABASE IF NOT EXISTS testdb';
     $retval = mysqli_query($conn, $sql);
     if (! $retval) {
-        die('Could not create database: ' . mysqli_error($conn)); // Added $conn here
+        die('Could not create database: ' . mysqli_error($conn));
     }
 
     echo 'Database testdb created successfully<br>';
 
-    $sql = 'CREATE TABLE employee( '.
-    'emp_id INT NOT NULL AUTO_INCREMENT, '.
-    'emp_name VARCHAR(20) NOT NULL, '.
-    'emp_address VARCHAR(20) NOT NULL, '.
-    'emp_salary INT NOT NULL, '.
-    'PRIMARY KEY (emp_id))'; 
-
+    // Create the employee table
     mysqli_select_db($conn, 'testdb');
+    $sql = 'CREATE TABLE IF NOT EXISTS employee (
+        emp_id INT NOT NULL AUTO_INCREMENT,
+        emp_name VARCHAR(20) NOT NULL,
+        emp_address VARCHAR(20) NOT NULL,
+        emp_salary INT NOT NULL,
+        PRIMARY KEY (emp_id)
+    )';
+
     $retval = mysqli_query($conn, $sql);
 
     if (! $retval ) {
-        die('Could not create table: ' . mysqli_error($conn)); // Added $conn here
+        die('Could not create table: ' . mysqli_error($conn));
     }
 
     echo 'Table employee created successfully<br>';
 
+    // Insert data into the employee table
     $sql = "INSERT INTO employee(emp_name, emp_address, emp_salary) VALUES('Rajiv', 'Nanded', 10000)";
 
-    mysqli_select_db($conn, 'testdb');
     $retval = mysqli_query($conn, $sql);
 
-    if (! $retval ) {
-        die('Could not enter data: ' . mysqli_error($conn)); // Added $conn here
+    if(! $retval ) {
+        die('Could not enter data: ' . mysqli_error($conn));
     }
 
     echo 'Entered data successfully<br>';
 
+    // Close the database connection
     mysqli_close($conn);
 
     ?>
